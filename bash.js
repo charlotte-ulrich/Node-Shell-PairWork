6ad23bc5d60e01b479a4cd0c3bb22c51efd65833
Charlotte Ulrich <charlotte.ulrich93@gmail.com>
2021-03-05 15:31:16 -0600

    Finished Modules and Started Bonus Content

--- bash.js
+++ bash.js
@@ -1,11 +1,60 @@
-process.stdout.write('prompt > ');
+// const { fstat } = require("node:fs");
+const fs = require("fs");
+const fsPromises = require('fs').promises;
 
-process.stdin.on('data', (data) => {
+process.stdout.write("prompt > ");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+  const pwd = process.cwd();
+  if (cmd === "pwd") {
+    process.stdout.write(pwd);
+  }
+  if (cmd === "ls") {
+    fs.readdir("./", "utf8", (err, files) => {
+      if (err) {
+        throw err;
+      } else {
+        process.stdout.write(files.join("\n"));
+        process.stdout.write("prompt > ");
+      }
+    });
+  }
+//   if (cmd === 'cat') {
+//     fs.promises.readFile("./bash").then(function(result) { 
+//     process.stdout.write('' + result); 
+// }) 
+// .catch(function(error) { 
+//    process.stdout.write((error); 
+// })
+// );
+//   }
+
+module.exports = function () {
+  process.stdin.on("data", (data) => {
     const cmd = data.toString().trim();
-    const pwd = process.cwd()
-    if (cmd === 'pwd'){
-        process.stdout.write(pwd);
+    const pwd = process.cwd();
+    if (cmd === "pwd") {
+      process.stdout.write(pwd);
     }
-    process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
-})
+    process.stdout.write("You typed: " + cmd);
+  });
+};
+
+module.exports = function () {
+  process.stdin.on("data", (data) => {
+    const cmd = data.toString().trim();
+
+    if (cmd === "ls") {
+      fs.readdir("./", "utf8", (err, files) => {
+        if (err) {
+          throw err;
+        } else {
+          process.stdout.write(files.join("\n"));
+          process.stdout.write("prompt > ");
+        }
+      });
+    }
+    process.stdout.write("You typed: " + cmd);
+  });
+};
