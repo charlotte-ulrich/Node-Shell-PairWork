615491c736ea47a5ef4af131266625a4cc75e60c
lekaren-web <karenn02163@gmail.com>
2021-03-05 15:26:33 -0500

    pwd code done

--- bash.js
+++ bash.js
@@ -1,9 +1,11 @@
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
-
     const cmd = data.toString().trim();
-
+    const pwd = process.cwd()
+    if (cmd === 'pwd'){
+        process.stdout.write(pwd);
+    }
     process.stdout.write('You typed: ' + cmd);
     process.stdout.write('\nprompt > ');
 })
